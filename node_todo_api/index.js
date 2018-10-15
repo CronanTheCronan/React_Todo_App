7f63f60b7f21c7b7bfeb0a7471861a382a176655
mattcronan.webdev <mattcronan.webdev@gmail.com>
2018-10-15 15:47:50 -0500

    Added TodoItem component functionality

--- node_todo_api/index.js
+++ node_todo_api/index.js
@@ -15,6 +15,6 @@ app.get('/', function(req, res){
         res.sendFile('index.html');
 })
 
-app.listen(3000, function(){
+app.listen(3001, function(){
         console.log("I'm Tiny Rick!");
 })
--- todos_frontend/src/TodoList.js
+++ todos_frontend/src/TodoList.js
@@ -1,9 +1,50 @@
 import React, {Component} from 'react';
+import TodoItem from './TodoItem';
+const APIURL = '/api/todos';
 
 class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            todos: []
+        }
+    }
+
+    componentWillMount(){
+        this.loadTodos();
+    }
+
+    loadTodos(){
+        fetch(APIURL)
+        .then(resp => {
+            if(!resp.ok) {
+                if(resp.status >= 400 && resp.status < 500){
+                    return resp.json().then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    })
+                } else {
+                    let err = {errorMessage: 'Please try again later, server not responding.'}
+                }
+            }
+            return resp.json();
+        })
+        .then(todos => this.setState({todos}));
+    }
     render(){
+        const todos = this.state.todos.map((t) => (
+            <TodoItem
+                key={t._id}
+                {...t}
+            />
+        ));
         return(
+        <div>
         <h1>Todo List!</h1>
+        <ul>
+            {todos}
+        </ul>
+        </div>
         )
     }
 }
